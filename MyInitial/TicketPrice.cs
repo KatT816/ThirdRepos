ca3d1aa2b27c816c9ddcd3a5e3bb627bfab21fcb
001163159 <Kateryna.Tyshchenko@student.tafesa.edu.au>
2025-02-27 22:48:22 +1030

    commit the child discount changes

---
+++ MyInitial/TicketPrice.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ticketing
+{
+    public class TicketPrice
+    {
+        private int section;
+        private int quantity;
+        private bool discount;
+        private decimal amountDue;
+        private decimal mPrice;
+        private bool childDiscount; // Added child discount field
+
+        const decimal mdecBalcony = 35.5m;
+        const decimal mdecGeneral = 28.75m;
+        const decimal mdecBox = 62.0m;
+        const decimal mdecDiscount = 5.0m;
+        const decimal mdecChildDiscount = 1.0m; // 10% child discount
+
+        private int Section
+        {
+            get { return section; }
+            set { section = value; }
+        }
+
+         private int Quantity
+        {
+            get { return quantity; }
+            set { quantity = value; }
+        }
+
+         private bool Discount
+        {
+            get { return discount; }
+            set { discount = value; }
+        }
+
+        private bool ChildDiscount //Added Child Discount Property
+        {
+            get { return childDiscount; }
+            set { childDiscount = value; }
+        }
+
+        public decimal AmountDue
+        {
+            get { return amountDue; }
+            set { amountDue = value; }
+        }
+
+        // Constructor for TcicketPrice (modified to accept childDiscount)
+        public TicketPrice(int section, int quantity, bool discount, bool childDiscount)
+    {
+        Section = section;
+        Quantity = quantity;
+        Discount = discount;
+        ChildDiscount = childDiscount; // Added Child Discount
+        AmountDue = amountDue;
+    }
+
+     public void calculatePrice()
+     {
+
+         switch (section)
+         {
+             case 1:
+                 mPrice = mdecBalcony;
+                 break;
+             case 2:
+                 mPrice = mdecGeneral;
+                 break;
+             case 3:
+                 mPrice = mdecBox;
+                 break;
+         }
+         if (discount)
+         { mPrice -= mdecDiscount; }
+
+            if (childDiscount)
+            {
+                mPrice -= mdecChildDiscount; // Apply child discount
+            }
+
+
+            AmountDue = mPrice * quantity;
+
+     }
+    }
+}
